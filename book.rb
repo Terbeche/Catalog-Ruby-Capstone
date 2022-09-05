cf8e40b22ebaf6e18a3b206f20f7c4b3848b2aa1
Terbeche Mustapha <termus96@gmail.com>
2022-09-05 20:02:51 +0100

    Implement methods for Book and Label classes

--- book.rb
+++ book.rb
@@ -4,8 +4,12 @@ class Book < Item
   attr_accessor :publisher, :cover_state
 
   def initialize(publisher, cover_state, id, publish_date, archived)
-    super(id, publish_date, archived)
+    super(id, publish_date, archived = false)
     @publisher = publisher
     @cover_state = cover_state
+  end
+
+  def can_be_archived?()
+    super || cover_state == 'bad' ? true : false
   end
 end
--- label.rb
+++ label.rb
@@ -1,11 +1,16 @@
 class Label
   attr_accessor :title, :color
-  attr_reader :id,  :items
+  attr_reader :id, :items
 
-  def initialize(id, title, color, items)
-    @id = id
+  def initialize(title, color)
+    @id = rand 1000
     @title = title
     @color = color
-    @items = items
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.label = self
   end
 end
