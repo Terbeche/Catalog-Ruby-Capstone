b63e7ae88adbd7f12fbe1c9847487c98b7f6d574
HanningtoneM <hanningtonepompyy@gmail.com>
2022-09-07 15:25:41 +0300

    Add list_music_albums

--- music_album/music_album_controller.rb
+++ music_album/music_album_controller.rb
@@ -18,5 +18,22 @@ module MusicAlbumsController
     File.write('./music_album/music_albums.json', music_albums.to_json)
   end
 
+  def list_music_albums
+    puts '-' * 50
+    File.new('./music_album/music_albums.json', 'w+') unless File.exist?('./music_album/music_albums.json')
+
+    if File.empty?('./music_album/music_albums.json')
+      puts 'The music albums list is empty'
+    else
+      puts '🎶  Music albums list:'
+      data = File.read('./music_album/music_albums.json').split
+      music_albums = JSON.parse(data.join)
+      music_albums.each_with_index do |music_album, index|
+        puts "#{index + 1}-[Music album] ID: #{music_album['id']} | On spotify: #{music_album['on_spotify']} | " \
+             "Publish date: #{music_album['publish_date']} | Archived: #{music_album['archived']}"
+      end
+    end
+  end
+
   
 end
