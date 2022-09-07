9d82d8fde8c84a0493b2e830a2eaa6b49f18064f
HanningtoneM <hanningtonepompyy@gmail.com>
2022-09-07 15:22:57 +0300

    Add class MusicAlbum

---
+++ music_album/music_album.rb
@@ -0,0 +1,13 @@
+require 'json'
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify, publish_date, id = nil, archived: false)
+    super(publish_date, id, archived: archived)
+    @on_spotify = on_spotify
+  end
+
+  
+end
